c977144807dca7e76caccf999ddddbf228a80b7c
Arko Mandal <44357096+arkomandal@users.noreply.github.com>
2025-03-17 17:31:27 +0530

    Standalone component added

---
+++ src/app/my-standalone-component/my-standalone-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-standalone-component',
+  imports: [],
+  templateUrl: './my-standalone-component.component.html',
+  styleUrl: './my-standalone-component.component.scss',
+  standalone: true
+})
+export class MyStandaloneComponentComponent {
+
+}
